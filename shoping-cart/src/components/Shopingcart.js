d01bde6b4011b863893a7fccd5df8126d8d384c0
Christopher Potti <christopherpotti@PSADMINs-MacBook-Air.local>
2020-07-12 23:56:11 -0400

    this is the final version

--- shoping-cart/src/components/Shopingcart.js
+++ shoping-cart/src/components/Shopingcart.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 // import Items from './Items'
 
+
 export default class Shopingcart extends Component {
     constructor() {
         super()
         this.state = {
-           
         
         item:[
             {
@@ -24,7 +24,7 @@ export default class Shopingcart extends Component {
             quantity: 0
         }
     ],
-    cartSubTotal: 0,
+    cartSubTotal: 0 ,
     tax: 0.04,
     total:0,
         }  
@@ -44,51 +44,65 @@ export default class Shopingcart extends Component {
     render() {
     
     
-    let soap =this.state.item[0];
+    let soap  =this.state.item[0];
     let candy =this.state.item[1];
     let pizza =this.state.item[2];
 
-    let cartSubTotal = (pizza.quantity * pizza.price) + (soap.quantity * soap.price) + (candy.quantity * candy.price)
+    let cartSubTotal = (pizza.quantity * pizza.price.toFixed(2)) + (soap.quantity * soap.price.toFixed(2)) + (candy.quantity * candy.price.toFixed(2))
 
-    let tax = cartSubTotal * this.state.tax; 
+    let tax = cartSubTotal.toFixed(2) * this.state.tax.toFixed(2); 
 
     let sumTotalItem = soap.quantity + candy.quantity + pizza.quantity;
 
     let {quantity} = this.state.item[0];
 
+    let total = (cartSubTotal + tax).toFixed(2);
 
         return (
             <div className='wrapper'>
-                <h1> hello </h1>
                 
-                <div className='firstItems'>
-                {soap.name}
-                {soap.price}
+                {/* <div className='product'> */}
+                    <div className='firstItems'>
+                        <img />
+                        <img src='./assets/soap.jpg'/>
+                        {soap.name}
+                        {soap.price}
+                        <button className='btn' onClick={() => {this.addQuantity(0)}}> add to cart </button>
+                        {soap.quantity}
+                        <button className='btn' onClick={() => {this.subQuantity(0)}}> remove from cart </button>
+                    </div>
 
-                <button onClick={() => {this.addQuantity(0)}}> ++ </button>
 
-                {soap.quantity}
+                    <div className='secondItems'>
+                        <img src='./assets/candy1'/>
+                        {candy.name}
+                        {candy.price}
+                        <button className='btn' onClick={() => {this.addQuantity(1)}}> add to cart </button>
+                        {candy.quantity}
+                        <button className='btn' onClick={() => {this.subQuantity(1)}}> remove from cart </button>
+                    </div>
 
-                <button onClick={() => {this.subQuantity(0)}}> -- </button>
 
-                </div>
-                <div className='secondItems'>
-                {candy.name}
-                {candy.price}
-                {candy.quantity}
-                </div>
-                <div className='thirdItems'>
-                {pizza.name}
-                {pizza.price}
-                {pizza.quantity}
-                </div>
-                
+
+                    <div className='thirdItems'>
+                        <img src='./assets/pizza1.jpg'/>
+                        {pizza.name}
+                        {pizza.price}
+                        <button className='btn' onClick={() => {this.addQuantity(2)}}> add to cart </button>
+                        {pizza.quantity}
+                        <button className='btn' onClick={() => {this.subQuantity(2)}}> remove from cart </button>
+                    </div>
+
+                {/* <div/> */}
+
 
                 <div className='checkOut'>
+                <h1> Order Summary </h1>
 
-                    ({sumTotalItem} Items)
-                    subtotal:{cartSubTotal}
-                    Estimated:{tax}
+                    <h3> Items {sumTotalItem} </h3>
+                    <h3> Subtotal {cartSubTotal} </h3>
+                   <h3> Estimated {tax} </h3>
+                    <h3> Total {total} </h3>
                 </div>
 
 
